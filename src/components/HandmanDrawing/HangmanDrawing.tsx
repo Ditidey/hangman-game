27f4d8fab98448333f983ed28bd034c1e7253196
Diti Rani Dey <ditidey2017@gmail.com>
2023-08-11 12:42:29 +0100

    head and arms added

--- src/components/HandmanDrawing/HangmanDrawing.tsx
+++ src/components/HandmanDrawing/HangmanDrawing.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './Drawing.css';
 
+const Head = (
+    <div className='head'></div>
+)
+const Body = (
+    <div className='body-con'></div>
+)
+const RightArm = (
+    <div className='right-arm'></div>
+)
+const bodyPart = [];
+
 const HangmanDrawing = () => {
     return (
         <div className='body'>
+             {Head}
+             {Body}
+             {RightArm}
             {/* for hanger */}
-            <div style={{width: '10px', height:'100px', top:0, right: 0, position: 'absolute' ,background: 'black'}}></div>
+            <div style={{width: '10px', height:'80px', marginLeft:'260px', position:'absolute', top:0, background: 'black'}}></div>
             <div style={{width: '150px', height:'10px', marginLeft:'120px', background: 'blue'}}></div>
             <div style={{width: '10px', height:'400px', marginLeft:'120px', background: 'green'}}></div>
             <div style={{width: '250px', height:'10px', background: 'black'}}></div>
